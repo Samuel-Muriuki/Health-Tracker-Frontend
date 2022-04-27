ab1e184a27decfff436164d8555c463b0d7f92fb
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-04-27 12:40:20 +0300

    Creating the Angular Files

---
+++ src/app/forms/lab-form/lab-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lab-form',
+  templateUrl: './lab-form.component.html',
+  styleUrls: ['./lab-form.component.css']
+})
+export class LabFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
